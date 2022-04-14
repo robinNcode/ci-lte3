b2dfa5a07e2b8cf5a58aa96b3f255e584888e8c9
robinNcode <msmrobin518@gmail.com>
2022-04-14 14:46:52 +0600

    template on process

---
+++ app/Views/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 2.3.0
+    </div>
+    <strong>Copyright &copy; <?= date('Y')?> <a href="http://adovasoft.com">AdovaSoft</a>.</strong> All rights reserved.
+</footer>
